eef9f84278ea24f4f38476b86a14aa37b523625d
djbyrne <danbyrne8@gmail.com>
2019-01-11 00:44:15 -0600

    first swift commit

--- swift-fun/ViewController.swift
+++ swift-fun/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var buttonCount = 0
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+    @IBAction func buttonTapped1(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        print(buttonCount)
+        if buttonCount >= 10{
+        view.backgroundColor =  UIColor.red
+            myLabel.text = "You hit the button more than 10 times!"}
+        if buttonCount >= 15{
+            view.backgroundColor =  UIColor.green
+            myLabel.text = "You hit the button more than 15 times!"}
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
 
