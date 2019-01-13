11aad690fb2633f9bd9a84ba19fc4ff8eab36106
Dan Byrne <danbyrne8@gmail.com>
2019-01-12 20:16:45 -0600

    swift_fun final changes
    finalized

--- swift-fun/ViewController.swift
+++ swift-fun/ViewController.swift
@@ -10,23 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var buttonCount = 0
+var buttonCount = 0
 
     @IBOutlet weak var myLabel: UILabel!
     
+    @IBOutlet weak var TopTextField: UITextField!
+    
+    @IBOutlet weak var BottomTextField: UITextField!
+    
+    @IBOutlet weak var additionSwitch: UISwitch!
     
     @IBAction func buttonTapped1(_ sender: Any) {
         
-        buttonCount = buttonCount + 1
+        let addition = additionSwitch.isOn
+        
+        if addition {
+            let sum = Double(TopTextField.text!)! + Double(BottomTextField.text!)!
+            
+            myLabel.text = "\(TopTextField.text!) + \(BottomTextField.text!) = \(sum)"
+            
+        }
+        else{
+            let sum = Double(TopTextField.text!)! - Double(BottomTextField.text!)!
+            
+            myLabel.text = "\(TopTextField.text!) - \(BottomTextField.text!) = \(sum)"
+        }
+        
+        
+        
+    buttonCount = buttonCount + 1
+        
         print(buttonCount)
         if buttonCount >= 10{
         view.backgroundColor =  UIColor.red
-            myLabel.text = "You hit the button more than 10 times!"}
+            //myLabel.text = "You hit the button more than 10 times!"
+            
+        }
         if buttonCount >= 15{
             view.backgroundColor =  UIColor.green
-            myLabel.text = "You hit the button more than 15 times!"}
+            //myLabel.text = "You hit the button more than 15 times!"
+            
+        }
+ 
+ 
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
